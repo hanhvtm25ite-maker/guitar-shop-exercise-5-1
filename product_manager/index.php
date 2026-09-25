5bf299bf205264af0a48f76b614604af6cee6881
My Hanh <hanhvtm.25ite@vku.udn.vn>
2026-09-25 10:09:10 +0700

    Complete exercises 9 and 10

--- product_manager/index.php
+++ product_manager/index.php
@@ -21,11 +21,13 @@ if ($action == 'list_products') {
     $categories = get_categories();
     $products = get_products_by_category($category_id);
     include('product_list.php');
+
 } else if ($action == 'delete_product') {
     $product_id = filter_input(INPUT_POST, 'product_id', 
             FILTER_VALIDATE_INT);
     $category_id = filter_input(INPUT_POST, 'category_id', 
             FILTER_VALIDATE_INT);
+
     if ($category_id == NULL || $category_id == FALSE ||
             $product_id == NULL || $product_id == FALSE) {
         $error = "Missing or incorrect product id or category id.";
@@ -34,9 +36,11 @@ if ($action == 'list_products') {
         delete_product($product_id);
         header("Location: .?category_id=$category_id");
     }
+
 } else if ($action == 'show_add_form') {
     $categories = get_categories();
     include('product_add.php');    
+
 } else if ($action == 'add_product') {
     $category_id = filter_input(INPUT_POST, 'category_id', 
             FILTER_VALIDATE_INT);
@@ -44,8 +48,9 @@ if ($action == 'list_products') {
     $name = filter_input(INPUT_POST, 'name');
     $price = filter_input(INPUT_POST, 'price');
 
-    if ($category_id == NULL || $category_id == FALSE || $code == NULL || 
-            $name == NULL || $price == NULL || $price == FALSE) {
+    if ($category_id == NULL || $category_id == FALSE || 
+            $code == NULL || $name == NULL || 
+            $price == NULL || $price == FALSE) {
         $error = "Invalid product data. Check all fields and try again.";
         include('../errors/error.php');
     } else { 
@@ -56,5 +61,28 @@ if ($action == 'list_products') {
 } else if ($action == 'list_categories') {
     $categories = get_categories();
     include('category_list.php');
+
+} else if ($action == 'add_category') {
+    $category_name = filter_input(INPUT_POST, 'category_name');
+
+    if ($category_name == NULL) {
+        $error = "Invalid category name. Check the name and try again.";
+        include('../errors/error.php');
+    } else {
+        add_category($category_name);
+        header('Location: .?action=list_categories');
+    }
+
+} else if ($action == 'delete_category') {
+    $category_id = filter_input(INPUT_POST, 'category_id',
+            FILTER_VALIDATE_INT);
+
+    if ($category_id == NULL || $category_id == FALSE) {
+        $error = "Missing or incorrect category id.";
+        include('../errors/error.php');
+    } else {
+        delete_category($category_id);
+        header('Location: .?action=list_categories');
+    }
 }
 ?>
