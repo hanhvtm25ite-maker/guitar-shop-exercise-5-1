a23c7bfe6a580b1d15866d836a8735a8855bf4ab
My Hanh <hanhvtm.25ite@vku.udn.vn>
2026-09-25 10:22:03 +0700

    Complete exercise 16

--- product_catalog/product_list.php
+++ product_catalog/product_list.php
@@ -1,33 +1,64 @@
 <?php include '../view/header.php'; ?>
 <main>
+    <h1>Product List</h1>
+
     <aside>
-        <h1>Categories</h1>
-        <nav>
-            <ul>
-                <!-- display links for all categories -->
-                <?php foreach($categories as $category) : ?>
-                <li>
-                    <a href="?category_id=<?php echo $category['categoryID']; ?>">
-                        <?php echo $category['categoryName']; ?>
-                    </a>
-                </li>
-                <?php endforeach; ?>
-        </ul>
-        </nav>
+        <!-- display a list of categories -->
+        <h2>Categories</h2>
+
+        <?php include '../view/categories_nav.php'; ?>
+
     </aside>
+
     <section>
-        <h1><?php echo $category_name; ?></h1>
-        <ul class="nav">
-            <!-- display links for products in selected category -->
+        <!-- display a table of products -->
+        <h2><?php echo $category_name; ?></h2>
+
+        <table>
+            <tr>
+                <th>Code</th>
+                <th>Name</th>
+                <th class="right">Price</th>
+                <th>&nbsp;</th>
+            </tr>
+
             <?php foreach ($products as $product) : ?>
-            <li>
-                <a href="?action=view_product&amp;product_id=<?php 
-                          echo $product['productID']; ?>">
-                    <?php echo $product['productName']; ?>
-                </a>
-            </li>
+            <tr>
+                <td><?php echo $product['productCode']; ?></td>
+
+                <td><?php echo $product['productName']; ?></td>
+
+                <td class="right">
+                    <?php echo $product['listPrice']; ?>
+                </td>
+
+                <td>
+                    <form action="." method="post">
+                        <input type="hidden" name="action"
+                               value="delete_product">
+
+                        <input type="hidden" name="product_id"
+                               value="<?php echo $product['productID']; ?>">
+
+                        <input type="hidden" name="category_id"
+                               value="<?php echo $product['categoryID']; ?>">
+
+                        <input type="submit" value="Delete">
+                    </form>
+                </td>
+            </tr>
             <?php endforeach; ?>
-        </ul>
+        </table>
+
+        <p>
+            <a href="?action=show_add_form">Add Product</a>
+        </p>
+
+        <p class="last_paragraph">
+            <a href="?action=list_categories">List Categories</a>
+        </p>
+
     </section>
 </main>
+
 <?php include '../view/footer.php'; ?>
